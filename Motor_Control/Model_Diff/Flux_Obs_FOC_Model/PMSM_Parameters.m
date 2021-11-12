6350f1cd0c1f846e936decc0cbdf19cf2e00d7bc
Charos945 <c1101272959@gmail.com>
2021-11-12 15:37:47 +0800

    新增Flux 观测，未调

--- Motor_Control/Model_Diff/Flux_Obs_FOC_Model/PMSM_Parameters.m
+++ Motor_Control/Model_Diff/Flux_Obs_FOC_Model/PMSM_Parameters.m
@@ -1,12 +1,12 @@
 %Motor Control
 %Set Motor Parameters%
 pmsm = struct;
-pmsm.RatedVoltage = 350.0;  %TBD
-pmsm.PolePairs = 4;
+pmsm.RatedVoltage = 24.0;  %TBD
+pmsm.PolePairs = 2;
 pmsm.StatorPhaseResistance = single(0.008); % RS
-pmsm.InductanceLd = single(0.00014);         % Ld
-pmsm.InductanceLq = single(0.0003115);        % Lq
-pmsm.FluxLinkage = single(0.170559);           %Flux
+pmsm.InductanceLd = single(0.0040);         % Ld
+pmsm.InductanceLq = single(0.0042);        % Lq
+pmsm.FluxLinkage = single(0.0260812);           %Flux
 pmsm.Jkm2=single(0.002);
 
 %Set Inverter Parameters%
@@ -20,7 +20,7 @@ inverter.Ts=1/inverter.PWM_frequency; %Ts
 inverter.Speed_PI_T =0.002;%s
 
 currentloop = struct;
-currentloop.wc = single(6000); %rad/s 电流环带宽
+currentloop.wc = single(8000); %rad/s 电流环带宽
 currentloop.kp_d = pmsm.InductanceLd * currentloop.wc;
 currentloop.ki_d = currentloop.wc*pmsm.StatorPhaseResistance;
 currentloop.kp_q = pmsm.InductanceLq * currentloop.wc;
